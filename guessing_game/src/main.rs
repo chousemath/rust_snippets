a5d22946a09f2da0da031a37540f3347225b875d
Joseph Sungpil Choi <chousemath@gmail.com>
2017-07-26 08:38:22 +0900

    added a few more comments explaining the rust code so far

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,6 +9,11 @@ fn main() {
     println!("\x1b[36;1mPlease input a number between 1 and 100!\x1b[0m");
 
     // process that input
+    // the let statement is used to create a new variable
+    // variables are mutable by default, `mut` allows variable to mutate
+    // String::new() returns a new instance of a String (standard library)
+    // :: indicates that new is an associated function of the String type (static method)
+    // the new instance of String is an empty string
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line...");
     println!("You just entered: \x1b[36;1m{}\x1b[0m", guess);
